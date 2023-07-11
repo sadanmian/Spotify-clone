beb6f13fa3b8c4e93b5ff03f2b06c6765adeef04
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-12 00:41:13 +0530

    Complete

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ let audioElement = new Audio('songs/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar')
 let gif = document.getElementById('gif')
+let masterSongName = document.getElementById('masterSongName')
+let songItem = Array.from(document.getElementsByClassName('songItem'))
 
 let songs = [
     {
-        songName: "Warriyo - Mortals [NCS Release]",
+        songName: "Warriyo - Mortals",
         filePath: "songs/1.mp3",
         coverPath: "covers/1.jpg"
     },
@@ -18,46 +20,51 @@ let songs = [
         coverPath: "covers/2.jpg"
     },
     {
-        songName: "DEAF KEV - Invincible [NCS Release]-320k",
+        songName: "DEAF KEV - Invincible ",
         filePath: "songs/3.mp3",
         coverPath: "covers/3.jpg"
     },
     {
-        songName: "Different Heaven & EH!DE - My Heart [NCS Release]",
+        songName: "Different Heaven & EH!DE - My Heart",
         filePath: "songs/4.mp3",
         coverPath: "covers/4.jpg"
     },
     {
-        songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release",
+        songName: "Janji-Heroes-Tonight-feat-Johnning",
         filePath: "songs/5.mp3",
         coverPath: "covers/5.jpg"
     },
     {
         songName: "Rabba - Salam-e-Ishq",
-        filePath: "songs/2.mp3",
+        filePath: "songs/6.mp3",
         coverPath: "covers/6.jpg"
     },
     {
         songName: "Sakhiyaan - Salam-e-Ishq",
-        filePath: "songs/2.mp3",
+        filePath: "songs/7.mp3",
         coverPath: "covers/7.jpg"
     },
     {
         songName: "Bhula Dena - Salam-e-Ishq",
-        filePath: "songs/2.mp3",
+        filePath: "songs/8.mp3",
         coverPath: "covers/8.jpg"
     },
     {
         songName: "Tumhari Kasam - Salam-e-Ishq",
-        filePath: "songs/2.mp3",
+        filePath: "songs/9.mp3",
         coverPath: "covers/9.jpg"
     },
     {
         songName: "Na Jaana - Salam-e-Ishq",
-        filePath: "songs/4.mp3",
+        filePath: "songs/10.mp3",
         coverPath: "covers/10.jpg"
     },
 ]
+
+songItem.forEach((element, i) => {
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath
+    element.getElementsByClassName('songName')[0].innerText = songs[i].songName
+})
 
 // Handle Play Pause Click
 masterPlay.addEventListener('click', () => {
@@ -84,5 +91,61 @@ audioElement.addEventListener('timeupdate', () => {
 
 myProgressBar.addEventListener('change', () => {
     audioElement.currentTime = (myProgressBar.value * audioElement.duration) / 100;
+})
+
+const makeAllPlays = () => {
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
+        element.classList.remove('fa-circle-pause')
+        element.classList.add('fa-circle-play')
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
+    element.addEventListener('click', (e) => {
+        makeAllPlays();
+        songIndex = parseInt(e.target.id)
+        e.target.classList.remove('fa-circle-play')
+        e.target.classList.add('fa-circle-pause')
+        audioElement.src = `songs/${songIndex + 1}.mp3`;
+        audioElement.currentTime = 0;
+        audioElement.play()
+        masterSongName.innerText = songs[songIndex].songName
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-circle-play')
+        masterPlay.classList.add('fa-circle-pause', 'fa-spin')
+    })
+})
+
+document.getElementById('next').addEventListener('click', () => {
+    if (songIndex >= 9) {
+        songIndex = 0
+    }
+    else {
+        songIndex = songIndex + 1;
+    }
+    audioElement.src = `songs/${songIndex + 1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName
+    audioElement.currentTime = 0;
+    audioElement.play()
+    gif.style.opacity = 1;
+    masterPlay.classList.remove('fa-circle-play')
+    masterPlay.classList.add('fa-circle-pause', 'fa-spin')
+
+})
+document.getElementById('previous').addEventListener('click', () => {
+    if (songIndex <= 0) {
+        songIndex = 9
+    }
+    else {
+        songIndex = songIndex - 1;
+    }
+    audioElement.src = `songs/${songIndex + 1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName
+    audioElement.currentTime = 0;
+    audioElement.play()
+    gif.style.opacity = 1;
+    masterPlay.classList.remove('fa-circle-play')
+    masterPlay.classList.add('fa-circle-pause', 'fa-spin')
+
 })
 
